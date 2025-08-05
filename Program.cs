af2b6578eade5b239ce59c2b88ea17572dfc4a3c
mikekyotn <michael.chankij@mssa.newhorizons.com>
2025-08-06 07:33:14 +0900

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Challenge_Lab_5._1
+{
+    //Given a non-empty array of integers nums, every element appears twice except for one.
+    //Find that single one.
+    //Status: WORKS BUT!! is Dictionary what they wanted us to use?
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 4, 1, 2, 1, 2 };
+
+            int answer = FindSingleton(MakeDictionary(nums));
+            
+            Console.WriteLine(answer);
+
+            Console.ReadKey();
+        }
+
+        static Dictionary<int, int> MakeDictionary(int[] nums)
+        {
+            Dictionary<int, int> countingDict = new();
+            foreach (int number in nums)
+            {
+                if (countingDict.ContainsKey(number))
+                {
+                    countingDict[number]++;
+                }
+                else
+                {
+                    countingDict.Add(number, 1);
+                }
+            }
+            return countingDict;
+        }
+
+        static int FindSingleton(Dictionary<int, int> dict)
+        {
+            int singleton = 0;
+            foreach (int number in dict.Keys)
+            {
+                if (dict[number] == 1)
+                {
+                    singleton =  number;
+                    break;
+                }
+            }
+
+            return singleton;
+                
+        }
+    }
+}
